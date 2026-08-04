526adcb6cda7d98dc13776d81e9e6af2fba72417
adiolk98 <adiolk98@users.noreply.github.com>
2026-08-04 05:52:03 +0000

    deploy: 451b7705166a29092cc907d3fbf99d22a888f2f1

---
+++ assets/PDFViewer-vwHi_VEy.js
@@ -0,0 +1,1 @@
+import{o as e,t}from"./index-D6GQwv1x.js";e();var n=t(),r=({filePath:e})=>(0,n.jsx)(`div`,{style:{width:`100%`,height:`100%`},children:(0,n.jsx)(`iframe`,{src:e,width:`100%`,height:`100%`,style:{border:`none`},title:`PDF Viewer`})});export{r as default};
